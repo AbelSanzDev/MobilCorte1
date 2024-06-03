975855e45101e4a84c230d88a98880bb714bc8ee
Abel Sánchez <abelsandev@gmail.com>
2024-06-03 13:19:47 -0700

    Se realizaron cambios

--- app/src/main/java/com/example/appholamundo2/MenuActivity.java
+++ app/src/main/java/com/example/appholamundo2/MenuActivity.java
@@ -58,6 +58,13 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        crvSpinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, SpinnerPersonalizado.class);
+                startActivity(intent);
+            }
+        });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
