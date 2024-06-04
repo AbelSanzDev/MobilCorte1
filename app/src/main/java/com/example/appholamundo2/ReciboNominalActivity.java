8f704b7e8ab7ee129a7cc7c029fe20ad29f57343
Abel Sánchez <abelsandev@gmail.com>
2024-06-04 12:38:50 -0700

    Se configuración los valúes y se cargó la imagen en los recursos R.

--- app/src/main/java/com/example/appholamundo2/ReciboNominalActivity.java
+++ app/src/main/java/com/example/appholamundo2/ReciboNominalActivity.java
@@ -1,5 +1,6 @@
 package com.example.appholamundo2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,31 +16,80 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ReciboNominalActivity extends AppCompatActivity {
     private ReciboNomina recibo;
-    private TextView tvTituloCalculo;
-    private TextView tvNombreTrabajadorCalculo;
-    private TextView tvNombreTrabajador;
-    private EditText etNumeroRecibo;
-    private EditText etHorasTrabNormal;
-    private EditText etHorasExtras;
+    private TextView tvTituloCalculo,tvNombreTrabajadorCalculo,etNombre;
+    private EditText etNumeroRecibo,etHorasTrabNormal,etHorasExtras;
     private RadioGroup rgPuesto;
-    private RadioButton rbAuxiliar;
-    private RadioButton rbAlbanil;
-    private RadioButton rbIngObra;
-    private TextView tvSubtotal;
-    private TextView tvImpuesto;
-    private TextView tvTotalPagar;
-    private Button btnCalcular;
-    private Button btnLimpiar;
-    private Button btnRegresar;
+    private RadioButton rbAuxiliar,rbAlbanil,rbIngObra;
+    private TextView tvSubtotal,tvImpuesto,tvTotalPagar;
+    private Button btnCalcular,btnLimpiar,btnRegresar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recibo_nominal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        recibo = new ReciboNomina();
+
+        tvTituloCalculo = findViewById(R.id.tvTituloCalculo);
+        tvNombreTrabajadorCalculo = findViewById(R.id.tvNombreTrabajadorCalculo);
+        etNombre = findViewById(R.id.etNombre);
+        etNumeroRecibo = findViewById(R.id.etNumeroRecibo);
+        etHorasTrabNormal = findViewById(R.id.etHorasTrabajadas);
+        etHorasExtras = findViewById(R.id.etHorasExtras);
+        rgPuesto = findViewById(R.id.rgPuesto);
+        rbAuxiliar = findViewById(R.id.rbAuxiliar);
+        rbAlbanil = findViewById(R.id.rbAlbanil);
+        rbIngObra = findViewById(R.id.rbIngObra);
+        tvSubtotal = findViewById(R.id.tvSubtotal);
+        tvImpuesto = findViewById(R.id.tvImpuesto);
+        tvTotalPagar = findViewById(R.id.tvTotalPagar);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnLimpiar = findViewById(R.id.btnLimpiar);
+        btnRegresar = findViewById(R.id.btnRegresar);
+
+        Intent intent = getIntent();
+        String nombreTrabajador = intent.getStringExtra("nombreTrabajador");
+        tvNombreTrabajadorCalculo.setText(nombreTrabajador);
+        etNombre.setText(nombreTrabajador);
+
+        btnCalcular.setOnClickListener(view -> calcularNomina());
+        btnLimpiar.setOnClickListener(view -> limpiarCampos());
+        btnRegresar.setOnClickListener(view -> finish());
+
+    }
+    private void calcularNomina() {
+        int numRecibo = Integer.parseInt(etNumeroRecibo.getText().toString());
+        String nombre = tvNombreTrabajadorCalculo.getText().toString();
+        float horasTrabNormal = Float.parseFloat(etHorasTrabNormal.getText().toString());
+        float horasTrabExtras = Float.parseFloat(etHorasExtras.getText().toString());
+
+        int puesto = 0;
+        if (rbAuxiliar.isChecked()) puesto = 1;
+        if (rbAlbanil.isChecked()) puesto = 2;
+        if (rbIngObra.isChecked()) puesto = 3;
+
+        recibo.setNumRecibo(numRecibo);
+        recibo.setNombre(nombre);
+        recibo.setHorasTrabNormal(horasTrabNormal);
+        recibo.setHorasTrabExtras(horasTrabExtras);
+        recibo.setPuesto(puesto);
+
+        float subtotal = recibo.calcularSubtotal();
+        float impuesto = recibo.calcularImpuesto();
+        float total = recibo.calcularTotal();
+
+        tvSubtotal.setText(String.valueOf(subtotal));
+        tvImpuesto.setText(String.valueOf(impuesto));
+        tvTotalPagar.setText(String.valueOf(total));
+    }
+
+    private void limpiarCampos() {
+        etNumeroRecibo.setText("");
+        etHorasTrabNormal.setText("");
+        etHorasExtras.setText("");
+        rgPuesto.clearCheck();
+        tvSubtotal.setText("");
+        tvImpuesto.setText("");
+        tvTotalPagar.setText("");
     }
 }
