9982d1d0bef2233946ef46b71a9b6e6f903cd746
Abel Sánchez <abelsandev@gmail.com>
2024-06-04 12:23:27 -0700

    Se realizo la clase Recibo Nomina

--- app/src/main/java/com/example/appholamundo2/ReciboNominalActivity.java
+++ app/src/main/java/com/example/appholamundo2/ReciboNominalActivity.java
@@ -1,6 +1,11 @@
 package com.example.appholamundo2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ReciboNominalActivity extends AppCompatActivity {
-
+    private ReciboNomina recibo;
+    private TextView tvTituloCalculo;
+    private TextView tvNombreTrabajadorCalculo;
+    private TextView tvNombreTrabajador;
+    private EditText etNumeroRecibo;
+    private EditText etHorasTrabNormal;
+    private EditText etHorasExtras;
+    private RadioGroup rgPuesto;
+    private RadioButton rbAuxiliar;
+    private RadioButton rbAlbanil;
+    private RadioButton rbIngObra;
+    private TextView tvSubtotal;
+    private TextView tvImpuesto;
+    private TextView tvTotalPagar;
+    private Button btnCalcular;
+    private Button btnLimpiar;
+    private Button btnRegresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
